db7366986a0d1ccef96c8d9d82b3f0c11e138bde
Yulon <yulon@me.com>
2014-11-18 15:54:42 +0800

    U

--- 2014/rainbow.js
+++ 2014/rainbow.js
@@ -1,61 +1,59 @@
-function Rainbow (updataSeed) {
+function Rainbow () {
 
-	var rgb = mkRGB(255, 0, 0);
+	var rbColor = mkColor(255, 0, 0);
 	var rbThis = this;
-	
-	setInterval(function(){
-		upColor(rgb);
-	}, updataSeed);
 
-	this.blend = function(imageData, preReplaceColor){
-		var xrgb = rgb;
-		var width = imageData.width*4;
+	this.blend = function(imageData, alpha){
+		var cColor = mkColor(rbColor[0], rbColor[1], rbColor[2]);
+		var a = alpha/255;
+		var pWidth = imageData.width*4;
 		var i;
-		for (var w = 0; w < width; w+=4) {
+		for (var w = 0; w < pWidth; w+=4) {
 			for (var h = 0; h < imageData.height; h++) {
-				i = width * h + w;
-				if (imageData.data[i]==preReplaceColor[0] && imageData.data[i+1]==preReplaceColor[1] && imageData.data[i+2]==preReplaceColor[2] && imageData.data[i+3] != 0) {
-					imageData.data[i] = xrgb[0];
-					imageData.data[i+1] = xrgb[1];
-					imageData.data[i+2] = xrgb[2];
+				i = pWidth * h + w;
+				if (imageData.data[i+3] != 0) {
+					imageData.data[i]=a*cColor[0] + (1-a)*imageData.data[i]
+					imageData.data[i+1]=a*cColor[1] + (1-a)*imageData.data[i+1]
+					imageData.data[i+2]=a*cColor[2] + (1-a)*imageData.data[i+2]
 				};
 			};
-			upColor(xrgb);
+			upColor(cColor);
 		};
 	};
 
-	this.blendFromCavctx = function(canvasContext, preReplaceColor) {
-		var imgd = canvasContext.getImageData(0, 0, cav.width, cav.height);
-		rbThis.blend(imgd, prc);
-		canvasContext.putImageData(imgd, 0, 0);
+	this.upStartColor = function(n) {
+		for (var i = 0; i < n; i++) {
+			upColor(rbColor);
+		};
 	};
+
 }
 
 function upColor (preUpdateColor) {
-	var next, up;
+	var next, last;
 	for (var i = 0; i < 3; i++) {
 		if(preUpdateColor[i]==255){
 			switch(i){
 				case 0:
 					next = 1;
-					up = 2;
+					last = 2;
 				break;
 				case 1:
 					next = 2;
-					up = 0;
+					last = 0;
 				break;
 				case 2:
 					next = 0;
-					up = 1;
+					last = 1;
 			}
 
-			if (preUpdateColor[up]>0) {
-				preUpdateColor[up]=preUpdateColor[up]-5;
+			if (preUpdateColor[last]>0) {
+				preUpdateColor[last]=preUpdateColor[last]-2;
 			} else {
 				if (preUpdateColor[next]==255) {
-					preUpdateColor[i]=preUpdateColor[i]-5;
+					preUpdateColor[i]=preUpdateColor[i]-2;
 				}else{
-					preUpdateColor[next]=preUpdateColor[next]+5;
+					preUpdateColor[next]=preUpdateColor[next]+2;
 				};
 			};
 			return;
@@ -64,10 +62,10 @@ function upColor (preUpdateColor) {
 	
 }
 
-function mkRGB (r, g, b) {
-	var rgb = new Uint8ClampedArray(3);
-	rgb[0] = r;
-	rgb[1] = g;
-	rgb[2] = b;
-	return rgb;
+function mkColor(r, g, b) {
+	var c = new Uint8ClampedArray(3);
+	c[0] = r;
+	c[1] = g;
+	c[2] = b;
+	return c;
 }
